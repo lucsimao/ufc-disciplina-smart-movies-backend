37cef77cdd4a184f562409d12be5132fd4b50f77
lucas <lucasimao64@gmail.com>
2020-12-05 20:37:11 -0300

    first: creating project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'indent': ['error', 2],
+    'no-multi-spaces': ['error'],
+    'no-console': ['error']
+  }
+};
