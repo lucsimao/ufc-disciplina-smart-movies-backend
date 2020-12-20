e42c9eb8e6dead90424ea304042060b0bfe9cb45
lucas <lucasimao64@gmail.com>
2020-12-19 22:15:32 -0300

    feat: configuring sequelize OMR to project

---
+++ src/config/db/db-config.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const database = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_URL,
+    dialect: process.env.DB_DIALECT,
+  },
+);
+
+module.exports = database;
