c31f8a57241c0011bd18108f6018ff75d81d4424
lucas <lucasimao64@gmail.com>
2021-03-16 22:21:04 -0300

    hotfix: container e rotas

--- src/controllers/movies.js
+++ src/controllers/movies.js
@@ -10,7 +10,7 @@ module.exports = app => {
     if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
 
     jwt.verify(token, process.env.SECRET, function(err, decoded) {
-      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+      if (err) return res.status(401).json({ auth: false, message: 'Failed to authenticate token.' });
 
       // se tudo estiver ok, salva no request para uso posterior
       req.userId = decoded.id;
